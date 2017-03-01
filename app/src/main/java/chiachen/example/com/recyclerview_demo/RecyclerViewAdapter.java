6815e0ad319f9f5445d455d0d00660a04ae41e0c
Jason_Chien <Jason_Chien@PerfectCorp.com>
2017-03-01 16:18:40 +0800

    Add animation

--- app/src/main/java/chiachen/example/com/recyclerview_demo/RecyclerViewAdapter.java
+++ app/src/main/java/chiachen/example/com/recyclerview_demo/RecyclerViewAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import java.util.Collections;
 import java.util.List;
@@ -36,7 +38,12 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<ViewHolder>{
 		holder.title.setText(list.get(position).title);
 		holder.description.setText(list.get(position).description);
 		holder.imageView.setImageResource(list.get(position).imageId);
-		//animate(holder);
+		animate(holder);
+	}
+
+	public void animate(RecyclerView.ViewHolder viewHolder) {
+		final Animation animAnticipateOvershoot = AnimationUtils.loadAnimation(context, R.anim.anticipate_overshoot);
+		viewHolder.itemView.setAnimation(animAnticipateOvershoot);
 	}
 
 	@Override
